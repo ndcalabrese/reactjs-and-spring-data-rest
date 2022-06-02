fa3784eefda4d1282634d3358ab4aeeda48be779
ndcalabrese <ndcalabrese@gmail.com>
2022-06-02 15:16:09 -0400

    Initial commit

---
+++ src/main/java/com/springtutorial/payroll/ReactjsAndSpringDataRestApplication.java
@@ -0,0 +1,15 @@
+package com.springtutorial.payroll;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReactjsAndSpringDataRestApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ReactjsAndSpringDataRestApplication.class, args);
+    }
+
+}
+
+
